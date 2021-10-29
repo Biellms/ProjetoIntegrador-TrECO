06f356a2ffbc274f6f815835ce1d5627c61220d0
Viegas <milton.viegas.br@gmail.com>
2021-10-28 22:40:27 -0300

    teste com react

---
+++ treco_reativo/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>TrECO</h1>
+      <p>Transformando treco em dindin!</p>
+      <div id="like_button_container"></div>
+    </div>
+
+  );
+  
+}
