9011a5dd674135dddb62f702cc7d697266f317a0
JeffreyRodriigues <jeffrey.rodriigues@gmail.com>
2021-11-04 14:07:02 -0300

    Implementando o Model Categoria no TrECO

---
+++ BackEnd/treco/src/main/java/br/org/generation/treco/TrecoApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.treco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrecoApplication.class, args);
+	}
+
+}
