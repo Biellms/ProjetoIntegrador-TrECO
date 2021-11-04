45ff534fe2274934af588f1cf044be7fd7842ee3
Biellms <biell.mendes8@gmail.com>
2021-11-04 11:43:50 -0300

    Implementando Model Categoria

---
+++ BackEnd/treco/src/test/java/br/org/generation/treco/TrecoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.generation.treco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
