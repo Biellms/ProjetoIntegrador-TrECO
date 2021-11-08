65f199ec8792e976c90c559a03de6ef154dcc27e
Viegas <milton.viegas.br@gmail.com>
2021-11-08 20:58:02 -0300

    add produto controller and repository

---
+++ BackEnd/treco/src/main/java/br/org/generation/treco/controller/ProdutoController.java
@@ -0,0 +1,91 @@
+package br.org.generation.treco.controller;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.org.generation.treco.model.Produto;
+import br.org.generation.treco.repository.ProdutoRepository;
+
+@RestController
+@RequestMapping("/produto")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+
+public class ProdutoController {
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Produto>> getAll(){ 
+		return ResponseEntity.ok(produtoRepository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> getById(@PathVariable long id){
+		return produtoRepository.findById(id)
+			.map(resposta -> ResponseEntity.ok(resposta))
+			.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/nomeProduto/{nomeProduto}")
+	public ResponseEntity<List<Produto>> getByTitulo(@PathVariable String nomeProduto){
+		return ResponseEntity.ok(produtoRepository.findAllByNomeProdutoContainingIgnoreCase(nomeProduto));
+	}
+	
+	@PostMapping 
+	public ResponseEntity<Produto> postProduto(@RequestBody Produto nomeProduto){ 
+		return ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(nomeProduto));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Produto> putProduto(@Valid @RequestBody Produto nomeProduto) {
+					
+		return produtoRepository.findById(nomeProduto.getId())
+				.map(resposta -> {
+					return ResponseEntity.ok().body(produtoRepository.save(nomeProduto));
+				})
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteProduto(@PathVariable long id) {
+		
+		return produtoRepository.findById(id)
+				.map(resposta -> {
+					produtoRepository.deleteById(id);
+					return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+				})
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/nomeProduto/{nomeProduto}/ouimagem/{imagem}")
+	public ResponseEntity<List<Produto>> getByNomeProdutoOuImagem(@PathVariable String nomeProduto, @PathVariable String imagem){
+		return ResponseEntity.ok(produtoRepository.findByNomeProdutoOrImagem(nomeProduto, imagem));
+	}
+
+	@GetMapping("/nomeProduto/{nomeProduto}/eimagem/{imagem}")
+	public ResponseEntity<List<Produto>> getByNomeProdutoEImagem(@PathVariable String nomeProduto, @PathVariable String imagem){
+		return ResponseEntity.ok(produtoRepository.findByNomeProdutoAndImagem(nomeProduto, imagem));
+	}
+	
+	@GetMapping("/preco_inicial/{inicio}/preco_final/{fim}")
+	public ResponseEntity<List<Produto>> getByPrecoEntre(@PathVariable BigDecimal inicio, @PathVariable BigDecimal fim){
+		return ResponseEntity.ok(produtoRepository.buscarProdutoEntre(inicio, fim));
+	}
+
+}
