1301d3ee9a9dcbfcdd9e8b7fb943cc0ed70ec06d
Dejsilva <dejs_silva@hotmail.com>
2021-11-09 11:22:03 -0300

    Atualizando controller usuario

--- BackEnd/treco/src/main/java/br/org/generation/treco/controller/UsuarioController.java
+++ BackEnd/treco/src/main/java/br/org/generation/treco/controller/UsuarioController.java
@@ -3,10 +3,15 @@ package br.org.generation.treco.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,5 +42,29 @@ public class UsuarioController {
 		return ResponseEntity.ok(usuarioRepository.findAllByUsuarioContainingIgnoreCase(usuario));	
 	}
 	
-
+	@PostMapping
+	public ResponseEntity<Usuario> postUsuario(@RequestBody Usuario usuario){
+		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(usuario));
+		
+	}
+	
+	@PutMapping
+	public ResponseEntity<Usuario> putUsuario(@RequestBody Usuario usuario){
+		return usuarioRepository.findById(usuario.getId())
+		.map(resposta ->  ResponseEntity.ok(usuarioRepository.save(usuario)))
+		.orElse(ResponseEntity.notFound().build());	
+	
 }
+	@DeleteMapping({"/{id}"})
+	public ResponseEntity <?> delete(@PathVariable long id) {
+		   return usuarioRepository.findById(id)
+		           .map(checagem -> {
+		        	   usuarioRepository.deleteById(id);
+		             return ResponseEntity.ok().build();
+		           	}).orElse(ResponseEntity.notFound().build());
+	
+}
+	
+}
+
+
