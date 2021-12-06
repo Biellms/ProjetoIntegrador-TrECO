468583a4ef1b33db87585eb265ca3a140e801d40
Biellms <biell.mendes8@gmail.com>
2021-12-06 16:21:37 -0300

    Impl Services e Models

--- FrontEnd/src/paginas/login/Login.tsx
+++ FrontEnd/src/paginas/login/Login.tsx
@@ -1,36 +1,70 @@
-import React from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import { Grid, Box, Typography, TextField, Button } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import './Login.css';
+import useLocalStorage from 'react-use-localstorage';
+import UserLogin from '../../models/UserLogin';
+import { login } from '../../service/Service';
 
 function Login() {
+
+    let history = useHistory();
+    const [token, setToken] = useLocalStorage('token');
+
+    const [userLogin, setUserLogin] = useState<UserLogin>({
+        id: 0,
+        usuario: '',
+        senha: '',
+        token: ''
+    })
+
+    function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+        setUserLogin({
+            ...userLogin,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    useEffect(() => {
+        if (token != '') {
+            history.push('/home')
+        }
+    }, [token])
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        try {
+            await login('usuarios/logar', userLogin, setToken)
+        } catch (error) {
+            alert('Dados do usuário inconsistentes. Erro ao logar!')
+        }
+    }
 
     return (
         <Grid justifyContent='center' alignItems='center'>
             <Grid alignItems='center' xs={12}>
-                <Box paddingX={50} margin={13}>
-                    <form>
+                <Box paddingX={80} margin={13}>
+                    <form onSubmit={onSubmit}>
                         <Typography variant='h2' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Login</Typography>
-                        <TextField id='seuemail' label='seuemail@gmail.com' variant='outlined' name='email' margin='normal' fullWidth />
-                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
+                        <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='Usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='Senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
                         <Box marginTop={3} textAlign='center'>
-                            <Link to='/home' className='text-decorator-none'>
-                                <Button className='buttom' fullWidth>
-                                    Login
-                                </Button>
-                            </Link>
+                            <Button type='submit' className='buttom' fullWidth>
+                                Login
+                            </Button>
                         </Box>
                     </form>
                     <Box display='flex' justifyContent='center' marginTop={1}>
-                    <Typography variant='subtitle1' gutterBottom align='center' className='font' >Esqueceu a senha?</Typography>
+                        <Typography variant='subtitle1' gutterBottom align='center' className='font' >Esqueceu a senha?</Typography>
                     </Box>
                     <Box display='flex' justifyContent='center' marginTop={5}>
                         <Box marginRight={1} display='flex' justifyContent='center'>
                             <Typography variant='subtitle1' gutterBottom align='center' className='font'>Não tem uma conta?</Typography>
                         </Box >
                         <Link to='/cadastro' className='text-decorator-none'>
-                        <Typography variant='subtitle1' gutterBottom align='center' className='login-link-cadastro'>Cadastre-se</Typography>
-                        </Link>                           
+                            <Typography variant='subtitle1' gutterBottom align='center' className='login-link-cadastro'>Cadastre-se</Typography>
+                        </Link>
                     </Box>
                 </Box>
             </Grid>
